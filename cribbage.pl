11a0786785a364bb42e9f5f56e377deb053e7505
yukebin <kevinyu890@gmail.com>
2024-08-30 19:13:10 +1000

    added comments

--- cribbage.pl
+++ cribbage.pl
@@ -1,31 +1,50 @@
-/** <module> Prolog documentation processor
+/** This module selects the best hand in Cribbage by maximising the 
+    expected value based on possible start cards; select_hand/3.
 
-Author:   Kevin Yu
-Purpose:  To similate and evaluate hands in the card game Cribbage.
+Author:     Kevin Yu
+Student ID: 1462539
+Subject:    COMP30020 Declarative Programming
+
+This module contains the implementation of a Cribbage hand selector. It provides 
+predicates that selects the best hand, maximising the expected value (points) 
+based on every possible start cards. Given the dealt cards, select_hand/3 
+returns the best hand that one should keep and the crib cards that should be 
+discarded.
+
+This module aims to be used with the full Cribbage game implementation, where 
+there will be a dealer and a player. One can use this module as to implement a 
+bot, or to provide hints to the player on which cards to keep and discard.
+
+The select_hand/3 predicate works on top of the hand_value/3 predicate, which 
+calculates the value of a hand based on the rules. This predicate will also be 
+useful for the full Cribbage game where one can use it as a scoring function.
 */
 
 :- use_module(library(clpfd)).
 
 %% card(+Rank, +Suit)
-% Definition of a card in the deck
+% Definition of a card in the deck, includes 1, 11, 12, 13 for 
+% ace, jack, queen, king mapping respectively
 card(Rank, Suit) :-
     member(Rank, 
     [ace, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, jack, 11, queen, 12, king, 13]),
     member(Suit, [clubs, diamonds, hearts, spades]).
 
 %% hand_value(+Hand, +Startcard, -Value)
-% Calculate the value of a hand
+% Calculates the value of a "full hand" with given Hand and Startcard
 hand_value(Hand, StartCard, Value) :-
     append(Hand, [StartCard], FullHand),
-    sort_hand(FullHand, SortedHand),
+    sort_hand(FullHand, SortedHand), % Sort the hand for easier scoring
     score_hand(SortedHand, Hand, StartCard, Value),
     !. % ! cuts off backtracking
 
 %% select_hand(+Cards, -Hand, -Cribcards)
-% Selects the best hand to maximise expected value
+% Selects the best Hand with given Cards (5 or 6 cards) and 
+% corresponding Cribcards
 select_hand(Cards, BestHand, BestCribcards) :-
-    findall(Hand-Cribcards, choose_hand(Cards, Hand, Cribcards), HandCribPairs),
-    best_hand(HandCribPairs, BestHand, BestCribcards).
+    % First, generate all possible hands and corresponding crib cards
+    bagof(Hand-Cribcards, choose_hand(Cards, Hand, Cribcards), HandCribPairs),
+    best_hand(HandCribPairs, BestHand, BestCribcards). % Find the best hand
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%% Card Value Definition %%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
@@ -64,7 +83,7 @@ card_value(card(king, _), 10).
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% Sort %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 %% sort_hand(+Hand, -SortedHand)
-% Sorts a hand by rank value
+% Sorts a hand by its rank value
 sort_hand(Hand, SortedHand) :-
     maplist(rank_value, Hand, HandValues),
     msort(HandValues, SortedHandValues),
@@ -83,8 +102,9 @@ score_hand(SortedHand, Hand, Startcard, Value) :-
     Value is Points15 + PointsPairs + PointsRuns + PointsFlushes + PointsNobs.
 
 %% score_15s(+Hand, -Points)
-% Calculate points for 15s in the hand
+% Calculate Points for 15s in the hand, given Hand
 score_15s(Hand, Points) :-
+    % Find all the valid combinations of cards that sum to 15
     (   bagof(Combination, valid_15s(Hand, Combination), Fifteens) ->  
         length(Fifteens, Length)
     ;   Length = 0
@@ -92,7 +112,7 @@ score_15s(Hand, Points) :-
     Points is Length * 2.
 
 %% score_pairs(+Hand, -Points)
-% Calculate points for pairs in the hand, assuming the hand is sorted
+% Calculate Points for pairs in the Hand, assuming the Hand is sorted
 score_pairs([], 0).
 score_pairs([card(Rank, _), card(Rank, _)|Rest], Points) :-
     % Check for three-of-a-kind
@@ -111,10 +131,9 @@ score_pairs([_|Rest], Points) :-
     score_pairs(Rest, Points).
 
 %% score_runs(+Hand, -Points)
-% Calculate points for runs in the hand
+% Calculate Points for runs in the Hand, assuming the Hand is sorted
 score_runs(SortedHand, Points) :-
     (   bagof(RunPoints, valid_run(SortedHand, RunPoints), RunPointsList) ->
-        findall(RunPoints, valid_run(SortedHand, RunPoints), RunPointsList),
         % We only want the longest run
         max(RunPointsList, Max),
         count_occurrences(RunPointsList, Max, Occurrences),
@@ -123,8 +142,10 @@ score_runs(SortedHand, Points) :-
     ).
 
 %% score_flushes(+Hand, +StartCard, -Points)
-% Calculate points for flushes in the hand
+% Calculate Points for flushes in the hand
 score_flushes(Hand, StartCard, Points) :-
+    % 4 points if all hand cards are the same suit; 
+    % 1 more if the start card matches.
     (   same_suits([StartCard | Hand]) ->
         Points is 5
     ;   same_suits(Hand) ->
@@ -133,8 +154,9 @@ score_flushes(Hand, StartCard, Points) :-
     ).
 
 %% score_nobs(+Hand, +StartCard, -Points)
-% Calculate points for nobs in the hand
+% Calculate Points for nobs in the hand
 score_nobs(Hand, card(_, Suit), Points) :-
+    % a point if the hand contains a jack of the same suit as the start card
     (   member(card(jack, Suit), Hand) ->
         Points is 1
     ;   Points is 0
@@ -143,14 +165,14 @@ score_nobs(Hand, card(_, Suit), Points) :-
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%% Hand Selection Core %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 %% choose_hand(+Cards, -Hand, -Cribcards)
-% Generate all possible hands (4 cards) and corresponding crib cards
+% Generate all possible Hands (4 cards) and corresponding Cribcards
 choose_hand(Cards, Hand, Cribcards) :-
     length(Hand, 4),
     combination(Cards, Hand),
     subtract(Cards, Hand, Cribcards).
 
 %% best_hand(+HandCribPairs, -BestHand, -BestCribcards)
-% Find the best hand based on expected value
+% Find the best Hand based on expected value of all possible start cards
 best_hand([Hand-Cribcards | Rest], BestHand, BestCribcards) :-
     expected_value(Hand, Value),
     best_hand(Rest, Hand-Cribcards, Value, BestHand, BestCribcards).
@@ -160,6 +182,7 @@ best_hand([], BestHand-Cribcards, _, BestHand, Cribcards).
 best_hand([Hand-Cribcards | Rest], CurrentBestHand-CurrentBestCribcards, 
             CurrentBestValue, BestHand, BestCribcards) :-
     expected_value(Hand, Value),
+    % Update the best hand if the current hand has a higher expected value
     (   Value > CurrentBestValue ->
         best_hand(Rest, Hand-Cribcards, Value, BestHand, BestCribcards)
     ;   best_hand(Rest, CurrentBestHand-CurrentBestCribcards, CurrentBestValue, 
@@ -169,7 +192,7 @@ best_hand([Hand-Cribcards | Rest], CurrentBestHand-CurrentBestCribcards,
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% Helper functions %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 %% valid_15s(+Values, -Combination)
-% Generates all valid combinations of cards that sum to 15
+% Generates all valid Combinations of cards that sum to 15
 valid_15s(Hand, Combination) :-
     combination(Hand, Combination),
     sum_to_15(Combination).
@@ -184,9 +207,9 @@ sum_to_15(Combination) :-
 %% valid_run(+Hand, -RunPoints)
 % Generate all valid runs and return their point value
 valid_run(Hand, RunPoints) :-
-    combination(Hand, Subset),      % Find all subsets of the hand
+    combination(Hand, Subset),                 % Find all subsets of the hand
     length(Subset, Length),
-    Length >= 3,                    % Runs must be at least 3 cards
+    Length >= 3,                               % Runs must be at least 3 cards
     maplist(rank_value, Subset, SubsetValues), % Convert cards to values
     is_consecutive(SubsetValues),
     RunPoints = Length.
@@ -209,11 +232,13 @@ same_suits([card(_, Suit), card(_, Suit)|Rest]) :-
 %% expected_value(+Hand, -Value)
 % Calculate the expected value of a hand
 expected_value(Hand, Value) :-
-    findall(StartCard, possible_start_card(StartCard, Hand), PossibleStartCards),
+    % First, generate all possible start cards
+    bagof(StartCard, possible_start_card(StartCard, Hand), PossibleStartCards),
+    % Calculate the value of each possible start card and original Hand
     maplist(hand_value(Hand), PossibleStartCards, Values),
     sum_list(Values, Total),
     length(PossibleStartCards, Count),
-    Value is Total / Count.
+    Value is Total / Count. % Expected value is the average of possible values
 
 %% possible_start_card(-Card, +Hand)
 % Check if a card is a valid start card
@@ -224,12 +249,14 @@ possible_start_card(card(Rank, Suit), Hand) :-
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%% Generic Functions %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
+%% max(+List, -Max)
 % Find the maximum value in a list
 max([H | []], H).
 max([H | T], Max) :- 
     max(T, Max1), Max1 > H, Max is Max1; 
     Max is H.
 
+%% count_occurrences(+List, +Value, -Count)
 % Count occurrences of a value in a list
 count_occurrences([], _, 0).
 count_occurrences([H|T], Value, Count) :-
